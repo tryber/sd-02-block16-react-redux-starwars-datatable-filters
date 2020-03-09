3ba5916a40513815f41a4fb766e7ada51bfc2761
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-03-09 19:18:02 -0300

    Test CC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Test from './components/Test';
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Test text="Hello, world" />
       </header>
     </div>
   );
---
+++ src/components/Test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Test({ text }) {
+  return (
+    <div>This is a test: {text}</div>
+  )
+}
+
+export default Test;
