91c76e582ccff7e76c6333480abdf1461342e1a7
Douglas Henrique <doughrqe@gmail.com>
2020-03-10 15:31:13 -0300

    removendo informações inuteis

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Table from './components/Table';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <Table />
       </header>
     </div>
