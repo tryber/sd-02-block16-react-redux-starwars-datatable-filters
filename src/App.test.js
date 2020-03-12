7c6cdbcf65b6ca26637ec38f4b9da8fac61031df
phtf92 <phtf92@gmail.com>
2020-03-12 02:23:55 -0300

    travis

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store';
+
+
+test('renders learn react link', () => {
+  const { getByText } = render(<Provider store={store}><App /></Provider>);
+  const linkElement = getByText('Star Wars');
+  expect(linkElement).toBeInTheDocument();
+});
