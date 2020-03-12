002a7fe1d000228dec676e2ea59872d107957b64
phtf92 <phtf92@gmail.com>
2020-03-12 01:02:31 -0300

    CC

--- src/reducers/searchFilterReducer.js
+++ src/reducers/searchFilterReducer.js
@@ -18,6 +18,25 @@ const initialState = {
   selectors: ['coluna', 'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'],
 };
 
+function newFilter(action, state) {
+  const { column, comparison, valueComparison } = action;
+  const nextFilter = state.filters;
+  const nextSelector = state.selectors;
+  nextSelector.splice(nextSelector.indexOf(column), 1);
+  nextFilter[1].numericValues.column = 'coluna';
+  nextFilter[1].numericValues.comparison = '-';
+  nextFilter[1].numericValues.valueComparison = 0;
+  const novoFiltro = {
+    numericValues: {
+      column,
+      comparison,
+      valueComparison,
+    },
+  };
+  nextFilter.push(novoFiltro);
+  return { nextFilter, nextSelector };
+}
+
 export default function reducer(state = initialState, action) {
   // console.log(nextFilter);
   console.log(action);
@@ -35,21 +54,7 @@ export default function reducer(state = initialState, action) {
       return { ...state, filters: nextFilter };
     }
     case NEW_FILTER: {
-      const { column, comparison, valueComparison } = action;
-      const nextFilter = state.filters;
-      const nextSelector = state.selectors;
-      nextSelector.splice(nextSelector.indexOf(column), 1);
-      nextFilter[1].numericValues.column = 'coluna';
-      nextFilter[1].numericValues.comparison = '-';
-      nextFilter[1].numericValues.valueComparison = 0;
-      const novoFiltro = {
-        numericValues: {
-          column,
-          comparison,
-          valueComparison,
-        },
-      };
-      nextFilter.push(novoFiltro);
+      const { nextFilter, nextSelector } = newFilter(action, state);
       return { ...state, filters: nextFilter, selectors: nextSelector };
     }
     default:
