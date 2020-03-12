f91de7a984715fc187fd56ffd3ccadb3e91513de
EddyeBoy27 <lipe0503@gmail.com>
2020-03-12 12:05:21 -0300

    Criado Componentee Table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Table from './components/Table';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Table />
     </div>
   );
 }
---
+++ src/components/Table.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Table extends Component {
+
+  render() {
+    return (
+      <div>
+        <div>StarWars Datatable with Filters</div>
+      </div>
+    )
+  }
+}
