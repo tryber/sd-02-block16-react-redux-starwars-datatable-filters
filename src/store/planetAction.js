be8f2ef5593ec557c2f1bea1e61f19268a7e46d2
EddyeBoy27 <lipe0503@gmail.com>
2020-03-23 12:07:21 -0300

    CC Issues Fix

--- src/store/planetAction.js
+++ src/store/planetAction.js
@@ -31,18 +31,23 @@ function dataWithFilter(results, planet) {
   };
 }
 
+function filterResults(whosFilter, planet) {
+  const filteredResult = whosFilter.map((result) => {
+    const filter = (result.name.toUpperCase().includes(planet.toUpperCase()))
+  ? result
+  : [];
+    return filter;
+  });
+  return filteredResult;
+}
+
 const planetAction = (planet, dataMock, dataMockFilterOn, data) => (
   (dispatch) => {
     let whosFilter = data.results;
     if (dataMockFilterOn) {
       whosFilter = dataMock.results;
     }
-    const filteredResult = whosFilter.map((result) => {
-      const filter = (result.name.toUpperCase().includes(planet.toUpperCase()))
-    ? result
-    : [];
-      return filter;
-    });
+    const filteredResult = filterResults(whosFilter, planet);
     const filterWithoutUndefined = filteredResult.filter((element) => element.length !== 0);
     const planetCase = planet.charAt(0).toUpperCase() + planet.substring(1);
     if (dataMockFilterOn) {
