4fef1041c3243bfd61b152602651331aef27c469
Giorgia <giorgialeoniac@gmail.com>
2020-03-26 04:54:17 -0300

    Consertando último erro

---
+++ src/components/TableFilters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TableFilters extends React.Component {
+  render() {
+    return (
+      <div>
+        teste
+      </div>
+    );
+  }
+}
+
+
+export default TableFilters;
