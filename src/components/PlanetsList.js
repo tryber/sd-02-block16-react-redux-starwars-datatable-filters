d65679b8106792e5b5a8398c00067dff30e913bf
Giorgia <giorgialeoniac@gmail.com>
2020-03-26 04:12:32 -0300

    Recomeçando depois de apagar quase 20 commits

--- src/components/PlanetsList.js
+++ src/components/PlanetsList.js
@@ -1,56 +1,64 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-
-class PlanetsList extends React.Component {
-  render() {
-    const { results } = this.props;
+const PlanetsList = ({ results }) => {
+  function tableHead() {
     return (
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Rotation Period</th>
-              <th>Orbital Period</th>
-              <th>Diameter</th>
-              <th>Climate</th>
-              <th>Gravity</th>
-              <th>Terrain</th>
-              <th>Surface Water</th>
-              <th>Population</th>
-              <th>Films</th>
-              <th>Created</th>
-              <th>Edited</th>
-              <th>URL</th>
-            </tr>
-          </thead>
-          <tbody>
-            {results.map((planet) => (
-              <tr key={planet.name}>
-                <td>{planet.name}</td>
-                <td>{planet.rotation_period}</td>
-                <td>{planet.orbital_period}</td>
-                <td>{planet.diameter}</td>
-                <td>{planet.climate}</td>
-                <td>{planet.gravity}</td>
-                <td>{planet.terrain}</td>
-                <td>{planet.surface_water}</td>
-                <td>{planet.population}</td>
-                <td>{planet.films}</td>
-                <td>{planet.created}</td>
-                <td>{planet.edited}</td>
-                <td>{planet.url}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
       </div>
     );
   }
-}
+
+  function tableBody() {
+    return (
+      <tbody>
+        {results.map((planet) => (
+          <tr key={planet.name}>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+  return (
+    <div>
+      <table>
+        {tableHead()}
+        {tableBody()}
+      </table>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ data: { results } }) => ({ results });
-
 
 export default connect(mapStateToProps)(PlanetsList);
