a173b8c8503826fc39ff0f864914f7593a15173a
Giorgia <giorgialeoniac@gmail.com>
2020-04-09 17:59:56 -0300

    Antes de reiniciar

--- src/components/PlanetsList.js
+++ src/components/PlanetsList.js
@@ -27,27 +27,7 @@ const PlanetsList = ({ results }) => {
   }
 
   function tableBody() {
-    return (
-      <tbody>
-        {results.map((planet) => (
-          <tr key={planet.name}>
-            <td className="name-line">{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
-          </tr>
-        ))}
-      </tbody>
-    );
+    console.log(results)
   }
   return (
     <div>
