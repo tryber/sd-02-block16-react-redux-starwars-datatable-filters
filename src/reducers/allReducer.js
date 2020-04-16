767cb048e5a1b64a28041356e16552fcdb8e5697
richardwellerson <richardwellerson@gmail.com>
2020-04-16 12:23:16 -0300

    Refatorando o reducer

--- src/reducers/allReducer.js
+++ src/reducers/allReducer.js
@@ -9,6 +9,8 @@ import {
   SORT_COLUMN,
 } from '../actions';
 
+const ordered = { column: 'name', order: 'ASC' };
+
 const columns = [
   'population',
   'orbital_period',
@@ -17,7 +19,14 @@ const columns = [
   'surface_water',
 ];
 
-const ordered = { column: 'name', order: 'ASC' };
+const INITIAL_STATE = ({
+  isFetching: false,
+  columnsSelect: columns,
+  filtersToShow: [],
+  name: '',
+  numericValues: [],
+  sorted: ordered,
+});
 
 const newColumns = (allColumns, i) => {
   const prevColumns = [...allColumns];
@@ -29,66 +38,81 @@ const newFilters = (allFilters, i) => {
   return prevFilters.slice(0, i).concat(prevFilters.slice(i + 1));
 };
 
-const INITIAL_STATE = ({
+const requestSwapi = (state) => ({
+  ...state,
+  isFetching: true,
+});
+
+const requestSwapiSuccess = (state, { data: { results } }) => ({
+  ...state,
   isFetching: false,
-  columnsSelect: columns,
-  filtersToShow: [],
-  name: '',
-  numericValues: [],
-  sorted: ordered,
+  planetsData: results,
+  filteredData: results,
+});
+
+const requestSwapiFailure = (state, message) => ({
+  ...state,
+  isFetching: false,
+  error: message,
+});
+
+const filterPlanetsWithName = (state, action) => ({
+  ...state,
+  name: { name: action.name },
+  filteredData: action.filterData,
+});
+
+const filterNumbers = (state, action) => ({
+  ...state,
+  numericValues: [
+    ...state.numericValues,
+    action.filter,
+  ],
+  columnsSelect: newColumns(action.columns, action.index),
+});
+
+const removeNumericFilter = (state, action) => ({
+  ...state,
+  numericValues: newFilters(action.filters, action.index),
+  columnsSelect: [...state.columnsSelect, action.column],
+});
+
+const eraseSort = (state, action) => ({
+  ...state,
+  sorted: action.value,
+});
+
+const sortColumn = (state, action) => ({
+  ...state,
+  sorted: action.toSort,
 });
 
 const allReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case REQUEST_SWAPI:
-      return {
-        ...state,
-        isFetching: true,
-      };
+      return requestSwapi(state);
+
     case REQUEST_SWAPI_SUCCESS:
-      return {
-        ...state,
-        isFetching: false,
-        planetsData: action.data.results,
-        filteredData: action.data.results,
-      };
+      return requestSwapiSuccess(state, action);
+
     case REQUEST_SWAPI_FAILURE:
-      return {
-        ...state,
-        isFetching: false,
-        error: action.error,
-      };
+      return requestSwapiFailure(state, action.error);
+
     case FILTER_PLANETS_WITH_NAME:
-      return {
-        ...state,
-        name: { name: action.name },
-        filteredData: action.filterData,
-      };
+      return filterPlanetsWithName(state, action);
+
     case FILTER_NUMBERS:
-      return {
-        ...state,
-        numericValues: [
-          ...state.numericValues,
-          action.filter,
-        ],
-        columnsSelect: newColumns(action.columns, action.index),
-      };
+      return filterNumbers(state, action);
+
     case REMOVE_NUMERIC_FILTER:
-      return {
-        ...state,
-        numericValues: newFilters(action.filters, action.index),
-        columnsSelect: [...state.columnsSelect, action.column],
-      };
+      return removeNumericFilter(state, action);
+
     case ERASE_SORT:
-      return {
-        ...state,
-        sorted: action.value,
-      };
+      return eraseSort(state, action);
+
     case SORT_COLUMN:
-      return {
-        ...state,
-        sorted: action.toSort,
-      };
+      return sortColumn(state, action);
+
     default: return state;
   }
 };
